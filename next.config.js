e363a9ed1d7a26f6ef34b9f13510bad4e1e26502
arsaizdihar <arsadihar@gmail.com>
2021-07-19 13:19:06 +0700

    add api url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["lh3.googleusercontent.com", "s.gravatar.com", "pbs.twimg.com"],
+    domains: [
+      "lh3.googleusercontent.com",
+      "s.gravatar.com",
+      "pbs.twimg.com",
+      "twitterapi.arsaizdihar.site",
+    ],
   },
 };
